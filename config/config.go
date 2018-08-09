f76c39657b35334e5c3b676e12b1d2358c914ba7
Lawtech <584563542@qq.com>
2018-08-09 22:24:03 +0800

    viper读取配置文件

---
+++ config/config.go
@@ -0,0 +1,61 @@
+package config
+
+import (
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+	"log"
+	"strings"
+)
+
+/*
+__author__ = 'lawtech'
+__date__ = '2018/8/9 下午9:54'
+*/
+
+type Config struct {
+	Name string
+}
+
+func Init(cfg string) error {
+	c := Config{
+		Name: cfg,
+	}
+
+	// 初始化配置文件
+	if err := c.initConfig(); err != nil {
+		return err
+	}
+
+	c.watchConfig()
+
+	return nil
+}
+
+func (c *Config) initConfig() error {
+	if c.Name != "" {
+		viper.SetConfigFile(c.Name) // 如果指定了配置文件，则解析指定的配置文件
+	} else {
+		viper.AddConfigPath("conf") // 如果没有指定配置文件，则添加路径，解析默认的配置文件
+		viper.SetConfigName("config")
+	}
+	viper.SetConfigType("yaml")     // 设置配置文件格式为yaml
+	viper.AutomaticEnv()            // 读取匹配的环境变量
+	viper.SetEnvPrefix("APISERVER") // 读取环境变量的前缀为APISERVER
+	replacer := strings.NewReplacer(".", "_")
+	viper.SetEnvKeyReplacer(replacer)
+
+	// viper搜索路径，解析配置文件
+	if err := viper.ReadInConfig(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// 监控配置文件变化并热加载程序
+func (c *Config) watchConfig() {
+	viper.WatchConfig()
+	viper.OnConfigChange(func(in fsnotify.Event) {
+		log.Printf("Config file changed: %s", in.Name)
+	})
+}
--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"errors"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+	"go_projects/api_server/config"
 	"go_projects/api_server/router"
 	"log"
 	"net/http"
@@ -14,7 +17,22 @@ __author__ = 'lawtech'
 __date__ = '2018/8/9 下午6:00'
 */
 
+var (
+	cfg = pflag.StringP("config", "c", "", "apiserver config file path.")
+)
+
 func main() {
+	pflag.Parse()
+
+	// init config
+	if err := config.Init(*cfg); err != nil {
+		panic(err)
+	}
+
+	// set gin mode
+	// gin 有 3 种运行模式：debug、release 和 test
+	gin.SetMode(viper.GetString("runmode"))
+
 	// create the Gin engine
 	g := gin.New()
 
@@ -32,15 +50,15 @@ func main() {
 		log.Print("The router has been deployed successfully.")
 	}()
 
-	log.Printf("Start to listening the incoming requests on http address: %s", ":8080")
-	log.Printf(http.ListenAndServe(":8080", g).Error())
+	log.Printf("Start to listening the incoming requests on http address: %s", viper.GetString("addr"))
+	log.Printf(http.ListenAndServe(viper.GetString("addr"), g).Error())
 }
 
 // pingServer pings the http server to make sure the router is working.
 func pingServer() error {
-	for i := 0; i < 2; i++ {
+	for i := 0; i < viper.GetInt("max_ping_count"); i++ {
 		// Ping the server by sending a GET request to `/health`.
-		resp, err := http.Get("http://127.0.0.1:8080" + "/sd/health")
+		resp, err := http.Get(viper.GetString("url") + "/sd/health")
 		if err == nil && resp.StatusCode == 200 {
 			return nil
 		}
